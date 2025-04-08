bd044ea868a03ffe805c3ea02e02b3741d0d6cef
tanjilahamed99 <tanjil113355@gmail.com>
2025-04-08 22:12:12 +0600

    same form logic added

--- app/page.js
+++ app/page.js
@@ -16,7 +16,31 @@ export default function Home() {
   const [stepTwo, setStepTwo] = useState(false);
   const [stepThree, setStepThree] = useState(false);
 
-  console.log(watch("example"));
+  // console.log(watch("example"));
+
+  console.log(stepOne);
+  console.log(stepTwo);
+  console.log(stepThree);
+
+  const handleNext = () => {
+    if (stepOne) {
+      setStepOne(false);
+      return setStepTwo(true);
+    } else if (stepTwo) {
+      setStepTwo(false);
+      return setStepThree(true);
+    }
+  };
+
+  const handlePrev = () => {
+    if (stepOne) {
+      setStepOne(false);
+      return setStepTwo(true);
+    } else if (stepTwo) {
+      setStepTwo(false);
+      return setStepThree(true);
+    }
+  };
 
   return (
     <div className="container mx-auto  flex items-center justify-center h-[100vh] text-black">
@@ -26,44 +50,54 @@ export default function Home() {
       >
         {/* register your input into the hook by invoking the "register" function */}
 
-        <div>
-          <h2 className="font-medium mb-1">Full Name</h2>
-          <input
-            className="input bg-white text-black border-black"
-            defaultValue="fullName"
-            {...register("fullName")}
-            placeholder="ex: Tanjil"
-          />
-        </div>
-        <div>
-          <h2 className="font-medium mb-1">Email</h2>
-          <input
-            className="input bg-white text-black border-black"
-            defaultValue="email"
-            {...register("email" , {required: true })}
-            type="email"
-            placeholder="example@gmail.com"
-          />
-        </div>
-        <div>
-          <h2 className="font-medium mb-1">Phone Number</h2>
-          <input
-            className="input bg-white text-black border-black"
-            defaultValue="phoneNumber"
-            {...register("phoneNumber" , {required: true }) }
-            type="number"
-            placeholder="+88"
-          />
-        </div>
+        {stepOne && (
+          <>
+            <div>
+              <h2 className="font-medium mb-1">Full Name</h2>
+              <input
+                className="input bg-white text-black border-black"
+                defaultValue=""
+                {...register("fullName")}
+                placeholder="ex: Tanjil"
+              />
+            </div>
+            <div>
+              <h2 className="font-medium mb-1">Email</h2>
+              <input
+                className="input bg-white text-black border-black"
+                defaultValue=""
+                {...register("email", { required: true })}
+                type="email"
+                placeholder="example@gmail.com"
+              />
+            </div>
+            <div>
+              <h2 className="font-medium mb-1">Phone Number</h2>
+              <input
+                className="input bg-white text-black border-black"
+                defaultValue=""
+                {...register("phoneNumber", { required: true })}
+                type="number"
+                placeholder="+88"
+              />
+            </div>
+          </>
+        )}
+        {stepTwo && <></>}
+        {stepThree && <></>}
 
         {/* errors will return when field validation fails  */}
         {errors.exampleRequired && <span>This field is required</span>}
 
         <div className="flex items-center justify-between">
-          <button className="btn btn-outline" type="submit">
+          <button className="btn btn-outline" type="button">
             Prev
           </button>
-          <button className="btn btn-outline" type="submit">
+          <button
+            className="btn btn-outline"
+            onClick={handleNext}
+            type="button"
+          >
             Next
           </button>
         </div>
